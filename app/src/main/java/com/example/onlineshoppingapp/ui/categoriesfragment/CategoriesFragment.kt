719aa137072769443134caced71bc971475a5335
Pareeesa <parisa.faridi80@gmail.com>
2022-06-07 17:17:31 +0430

    Navigate to CategoryFragment when a Category is clicked and send its id to CategoryFragment

--- app/src/main/java/com/example/onlineshoppingapp/ui/categoriesfragment/CategoriesFragment.kt
+++ app/src/main/java/com/example/onlineshoppingapp/ui/categoriesfragment/CategoriesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.onlineshoppingapp.adapters.CategoryAdapter
 import com.example.onlineshoppingapp.databinding.FragmentCategoriesBinding
@@ -28,7 +29,14 @@ class CategoriesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val categoriesAdapter = CategoryAdapter{}
+        val categoriesAdapter = CategoryAdapter{ category ->
+            val action = category.id?.let { id ->
+                CategoriesFragmentDirections.actionCategoriesFragmentToCategoryFragment(id)
+            }
+            if (action != null) {
+                findNavController().navigate(action)
+            }
+        }
         binding.rvCategories.apply {
             layoutManager = GridLayoutManager(requireContext(),2)
             adapter = categoriesAdapter
