4b181111710149d29d06002e653687fde6c5651f
Pareeesa <parisa.faridi80@gmail.com>
2022-06-08 11:34:54 +0430

    Add viewPager for showing images of the product

---
+++ app/src/main/java/com/example/onlineshoppingapp/adapters/ImageViewPagerAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.onlineshoppingapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.onlineshoppingapp.R
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.onlineshoppingapp.data.model.Image
+import com.example.onlineshoppingapp.databinding.ImageViewBinding
+
+class ImageViewPagerAdapter(private val imageList: List<Image>) :
+    RecyclerView.Adapter<ImageViewPagerAdapter.ViewPagerViewHolder>() {
+
+    inner class ViewPagerViewHolder(val binding: ImageViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun setData(imageUrl: String) {
+            Glide.with(binding.root.context)
+                .load(imageUrl)
+                .error(R.drawable.ic_baseline_error_24)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(binding.imageView)
+        }
+    }
+    override fun getItemCount(): Int = imageList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+
+        val binding = ImageViewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewPagerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+
+        holder.setData(imageList[position].src)
+    }
+
+}
--- app/src/main/java/com/example/onlineshoppingapp/ui/detailfragment/DetailFragment.kt
+++ app/src/main/java/com/example/onlineshoppingapp/ui/detailfragment/DetailFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
+import androidx.viewpager2.widget.ViewPager2
+import com.example.onlineshoppingapp.adapters.ImageViewPagerAdapter
 import com.example.onlineshoppingapp.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import me.relex.circleindicator.CircleIndicator3
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
@@ -17,6 +19,7 @@ class DetailFragment : Fragment() {
     private val detailViewModel : DetailViewModel by viewModels()
     private lateinit var binding : FragmentDetailBinding
     private val args :DetailFragmentArgs by navArgs()
+    private lateinit var imageViewPagerAdapter: ImageViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +33,33 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         detailViewModel.getProduct(args.productId).observe(viewLifecycleOwner){
             binding.product = it
-            Glide.with(binding.imageView.context).load(it.images?.get(0)?.src)
-                .into(binding.imageView)
+            imageViewPagerAdapter = it.images?.let { it1 -> ImageViewPagerAdapter(it1) }!!
+            setUpViewPager()
         }
+    }
+    private fun setUpViewPager() {
+
+        binding.viewPager.adapter = imageViewPagerAdapter
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val dotsIndicator = binding.dotsIndicator
+        dotsIndicator.attachTo(binding.viewPager)
+        val currentPageIndex = 0
+        binding.viewPager.currentItem = currentPageIndex
+
+        // registering for page change callback
+//        binding.viewPager.registerOnPageChangeCallback(
+//            object : ViewPager2.OnPageChangeCallback() {
+//                override fun onPageSelected(position: Int) {
+//                    super.onPageSelected(position)
+//                    //update the image number textview
+//                }
+//            }
+//        )
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.viewPager.unregisterOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {}
+        )
     }
 }
