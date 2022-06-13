9ffa8cc2a7c753f55bec346360a8556baed8a106
Pareeesa <parisa.faridi80@gmail.com>
2022-06-13 13:13:52 +0430

    Change the splash screen

---
+++ app/src/main/java/com/example/onlineshoppingapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.onlineshoppingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.onlineshoppingapp.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
