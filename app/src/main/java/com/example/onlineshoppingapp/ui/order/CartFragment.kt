f8a8170e9ce2f76ef63002509705d3ee179db2ac
Pareeesa <parisa.faridi80@gmail.com>
2022-06-22 11:34:07 +0430

    Implement the logic of cart fragment and its viewModel

--- app/src/main/java/com/example/onlineshoppingapp/ui/order/CartFragment.kt
+++ app/src/main/java/com/example/onlineshoppingapp/ui/order/CartFragment.kt
@@ -1,19 +1,77 @@
 package com.example.onlineshoppingapp.ui.order
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.onlineshoppingapp.R
+import com.example.onlineshoppingapp.Resource
+import com.example.onlineshoppingapp.adapters.CartAdapter
+import com.example.onlineshoppingapp.databinding.FragmentCartBinding
+import com.example.onlineshoppingapp.ui.getErrorMessage
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CartFragment : Fragment() {
+
+    private lateinit var binding: FragmentCartBinding
+    private val viewModel: CartViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getOrder()
+        val adapter = CartAdapter { lineItem,quantity ->
+            viewModel.updateQuantity(lineItem.id,quantity)
+        }
+        binding.rvCartProducts.adapter = adapter
+        viewModel.order.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+                is Resource.Success -> { response.data?.let {
+                    hideProgressBar()
+                    adapter.submitList(it.lineItems)
+                    binding.tvSumOfPrice.text = it.total
+                }
+                }
+                is Resource.Error -> {
+                    response.message?.let {  message ->
+                        response.code?.let { showErrorSnack(message, it) }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun hideProgressBar() {
+        binding.layout.visibility = View.VISIBLE
+        binding.lottie.visibility = View.GONE
+    }
+    private fun showProgressBar() {
+        binding.lottie.setAnimation(R.raw.loading)
+        binding.lottie.visibility = View.VISIBLE
+        binding.lottie.playAnimation()
+    }
+    private fun showErrorSnack(message: String, code: Int) {
+        val snackBar = Snackbar.make(binding.layout, getErrorMessage(message,code), Snackbar.LENGTH_INDEFINITE)
+        snackBar.setAction(
+            getString(R.string.try_again)
+        ) {
+            viewModel.getOrder()
+            binding.lottie.playAnimation()
+        }
+        snackBar.show()
     }
 }
---
+++ app/src/main/java/com/example/onlineshoppingapp/ui/order/CartViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.onlineshoppingapp.ui.order
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.onlineshoppingapp.R
+import com.example.onlineshoppingapp.Resource
+import com.example.onlineshoppingapp.data.Repository
+import com.example.onlineshoppingapp.data.model.Order
+import com.example.onlineshoppingapp.data.model.Product
+import com.example.onlineshoppingapp.hasInternetConnection
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(private val repository: Repository, app: Application) :
+    AndroidViewModel(app) {
+
+    val order =MutableLiveData<Resource<Order>>()
+
+    fun getOrder() = viewModelScope.launch {
+        if (repository.isOrderNew() == null){
+            order.postValue(Resource.Error(getApplication<Application>().getString(R.string.empty_cart),code=2))
+            return@launch
+        }
+        order.postValue(Resource.Loading())
+        if (hasInternetConnection()) {
+            viewModelScope.launch {
+                order.postValue(repository.getOrder(repository.isOrderNew()!!.id))
+            }
+        } else
+            order.postValue(Resource.Error(getApplication<Application>().getString(R.string.no_internet_error), code = 1))
+    }
+
+    fun updateQuantity(id: Int, quantity: Int) {
+        TODO("update order")
+    }
+}
