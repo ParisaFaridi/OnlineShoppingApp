0cd2c558f543b7db05a667ea2c97af0c2e1cac23
Pareeesa <parisa.faridi80@gmail.com>
2022-06-26 17:01:22 +0430

    Create new fragment for completing the order

---
+++ app/src/main/java/com/example/onlineshoppingapp/CompleteOrderFragment.kt
@@ -0,0 +1,79 @@
+package com.example.onlineshoppingapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.onlineshoppingapp.adapters.ImageViewPagerAdapter
+import com.example.onlineshoppingapp.data.model.Shipping
+import com.example.onlineshoppingapp.databinding.FragmentCompleteOrderBinding
+import com.example.onlineshoppingapp.databinding.FragmentDetailBinding
+import com.example.onlineshoppingapp.ui.detailfragment.DetailFragmentArgs
+import com.example.onlineshoppingapp.ui.detailfragment.DetailViewModel
+import com.example.onlineshoppingapp.ui.getErrorMessage
+import com.example.onlineshoppingapp.ui.order.CartViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CompleteOrderFragment : Fragment() {
+
+    private lateinit var binding: FragmentCompleteOrderBinding
+    private val cartViewModel: CartViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCompleteOrderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCompleteOrder.setOnClickListener {
+            cartViewModel.completeOrder(Shipping(address1 = binding.etAddress.text.toString(),
+            city = binding.etCity.text.toString(),
+            postcode = binding.etZipCode.text.toString()))
+        }
+
+        cartViewModel.order.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Loading -> {
+                    binding.layout.visibility = View.GONE
+                    binding.lottie.visibility = View.VISIBLE
+                }
+                is Resource.Success -> { response.data?.let {
+                    binding.layout.visibility = View.VISIBLE
+                    binding.lottie.visibility = View.GONE
+                    Toast.makeText(requireContext(), "سفارش با موفقیت ثبت شد!", Toast.LENGTH_LONG).show()
+                    findNavController().navigate(R.id.action_completeOrderFragment_to_cartFragment)
+                }
+                }
+                is Resource.Error -> {
+                    response.message?.let {  message ->
+                        response.code?.let { showErrorSnack(message, it) }
+                    }
+                }
+            }
+        }
+    }
+    private fun showErrorSnack(message: String, code: Int) {
+        val snackBar = Snackbar.make(binding.layout, getErrorMessage(message,code), Snackbar.LENGTH_INDEFINITE)
+        snackBar.setAction(
+            getString(R.string.try_again)
+        ) {
+            cartViewModel.completeOrder(Shipping(address1 = binding.etAddress.text.toString(),
+                city = binding.etCity.text.toString(),
+                postcode = binding.etZipCode.text.toString()))
+            binding.lottie.playAnimation()
+        }
+        snackBar.show()
+    }
+
+}
