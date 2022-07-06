8b4daafa546e195865e0df4b6865f33aa160591e
Pareeesa <parisa.faridi80@gmail.com>
2022-07-06 09:57:16 +0430

    Fix some warnings

--- app/src/main/java/com/example/onlineshoppingapp/room/OrderDataBase.kt
+++ app/src/main/java/com/example/onlineshoppingapp/room/OrderDataBase.kt
@@ -8,5 +8,5 @@ import com.example.onlineshoppingapp.data.model.OrderId
 @Database(entities = [OrderId::class,Address::class], version = 1)
 abstract class OrderDataBase : RoomDatabase(){
     abstract val orderDao:OrderDao
-    abstract val
+    abstract val addressDao :AddressDao
 }
