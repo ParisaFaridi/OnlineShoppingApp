547f9d82b035095c1e7e2a9fc33c2eac2018ccc8
Pareeesa <parisa.faridi80@gmail.com>
2022-07-06 20:49:27 +0430

    Add edit and delete buttons to review_item.xml

--- app/src/main/java/com/example/onlineshoppingapp/adapters/ReviewAdapter.kt
+++ app/src/main/java/com/example/onlineshoppingapp/adapters/ReviewAdapter.kt
@@ -1,20 +1,22 @@
 package com.example.onlineshoppingapp.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.onlineshoppingapp.R
 import com.example.onlineshoppingapp.data.model.Review
-import com.example.onlineshoppingapp.databinding.ProductItemBinding
 import com.example.onlineshoppingapp.databinding.ReviewItemBinding
-import java.text.NumberFormat
-import java.util.*
 
-class ReviewAdapter : ListAdapter<Review, ReviewAdapter.ItemHolder>(FormulaDiffCallBack) {
+typealias DeleteListener = (Review) -> Unit
+typealias EditListener = (Review) -> Unit
+
+class ReviewAdapter(val context:Context, private val deleteListener: DeleteListener, private val editListener: EditListener)
+    : ListAdapter<Review, ReviewAdapter.ItemHolder>(FormulaDiffCallBack) {
 
     object FormulaDiffCallBack: DiffUtil.ItemCallback<Review>() {
         override fun areItemsTheSame(oldItem: Review, newItem: Review): Boolean {
@@ -40,6 +42,18 @@ class ReviewAdapter : ListAdapter<Review, ReviewAdapter.ItemHolder>(FormulaDiffC
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
         holder.binding.review = getItem(position)
         holder.binding.tvReview.text = fixDescription(getItem(position).review)
+        val userInfoShared = context.getSharedPreferences(context.getString(R.string.user_info),Context.MODE_PRIVATE)
+        val email = userInfoShared.getString(context.getString(R.string.email_share),"")
+        if (getItem(position).reviewerEmail == email){
+            holder.binding.btnDelete.visibility = View.VISIBLE
+            holder.binding.btnEdit.visibility = View.VISIBLE
+        }
+        holder.binding.btnDelete.setOnClickListener {
+            deleteListener.invoke(getItem(position))
+        }
+        holder.binding.btnEdit.setOnClickListener {
+            editListener.invoke(getItem(position))
+        }
     }
     private fun fixDescription(description: String): String {
         return description.replace("</p>", "")
